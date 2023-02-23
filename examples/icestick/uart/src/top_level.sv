f3ded84c8daa70c76240d1a0debb1eb9edb797ff
Fischer Moseley <42497969+fischermoseley@users.noreply.github.com>
2023-02-23 18:13:15 -0500

    update icestick project structure to match the a7's

---
+++ examples/icestick/uart/src/top_level.sv
@@ -0,0 +1,33 @@
+`default_nettype none
+`timescale 1ns / 1ps
+
+`include "src/debug.v"
+
+module top_level (
+	input wire clk,
+
+	input wire rs232_rx_ttl,
+	output logic rs232_tx_ttl
+	);
+    
+    wire rst = 0;
+	
+    // Signal Generator
+    logic [7:0] count;
+    always_ff @(posedge clk) count <= count + 1;
+
+    // debugger
+    manta manta(
+        .clk(clk),
+        .rst(rst),
+        .larry(count[0]),
+        .curly(count[1]),
+        .moe(count[2]),
+        .shemp(count[3:0]),
+        
+        .rxd(rs232_rx_ttl),
+        .txd(rs232_tx_ttl));
+
+endmodule
+
+`default_nettype wire
