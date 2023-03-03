3ff4298e24f58a49466682a1f63f19163c7579d3
Fischer Moseley <42497969+fischermoseley@users.noreply.github.com>
2023-03-02 22:33:40 -0500

    works (kinda) on hardware

--- examples/nexys_a7/counter/src/top_level.sv
+++ examples/nexys_a7/counter/src/top_level.sv
@@ -4,29 +4,36 @@
 module top_level (
 	input wire clk,
 	input wire btnc,
-	input wire btnu,
-	input wire [15:0] sw,
 
 	output logic [15:0] led,
+    output logic ca, cb, cc, cd, ce, cf, cg,
+    output logic [7:0] an,
+
 	input wire uart_txd_in,
 	output logic uart_rxd_out
 	);
 	
-    // Signal Generator
-    logic [7:0] count;
-    always_ff @(posedge clk) count <= count + 1;
-
-    // debugger
+    // // Signal Generator
+    // logic [7:0] count;
+    // always_ff @(posedge clk) count <= count + 1;
+    
     manta manta(
         .clk(clk),
         .rst(btnc),
-        .larry(count[0]),
-        .curly(count[1]),
-        .moe(count[2]),
-        .shemp(count[3:0]),
-        
         .rxd(uart_txd_in),
-        .txd(uart_rxd_out));
+        .txd(uart_rxd_out)
+    );
+
+    assign led = manta.brx_mem_1_addr;
+
+    logic [6:0] cat;
+	assign {cg,cf,ce,cd,cc,cb,ca} = cat;
+    seven_segment_controller ss (
+        .clk_in(clk),
+        .rst_in(btnc),
+        .val_in( (manta.mem_1_btx_rdata << 16) | (manta.brx_mem_1_wdata) ),
+        .cat_out(cat),
+        .an_out(an));
 
 endmodule
 
---
+++ examples/nexys_a7/counter/src/uart_tx.v
@@ -0,0 +1,81 @@
+`default_nettype none
+`timescale 1ns / 1ps
+
+module uart_tx(
+	input wire clk,
+	input wire rst,
+	
+	input wire [DATA_WIDTH-1:0] axiid,
+	input wire axiiv,
+	output reg axiir,
+
+	output reg txd
+	);
+	
+	parameter DATA_WIDTH = 0;
+	parameter CLK_FREQ_HZ = 0;
+	parameter BAUDRATE = 0;
+
+	localparam BAUD_PERIOD = CLK_FREQ_HZ / BAUDRATE;
+
+	reg busy;
+	assign axiir = ~busy; 
+
+	reg [$clog2(BAUD_PERIOD) - 1:0] baud_counter;
+	reg [$clog2(DATA_WIDTH + 2):0] bit_index;
+	reg [DATA_WIDTH - 1:0] data_buf;
+
+	// make secondary logic for baudrate
+	always @(posedge clk) begin
+		if(rst) baud_counter <= 0;
+		else begin
+			baud_counter <= (baud_counter == BAUD_PERIOD - 1) ? 0 : baud_counter + 1;
+		end
+	end
+	
+	always @(posedge clk) begin
+		
+		// reset logic
+		if(rst) begin
+			bit_index <= 0;
+			busy <= 0;
+			txd <= 1; // idle high
+		end
+
+		// enter transmitting state logic
+		// don't allow new requests to interrupt current
+		// transfers
+		if(axiiv && ~busy) begin
+			busy <= 1;
+			data_buf <= axiid;
+		end
+
+
+		// transmitting state logic
+		else if(baud_counter == 0 && busy) begin
+
+			if (bit_index == 0) begin
+				txd <= 0;
+				bit_index <= bit_index + 1;
+			end
+
+			else if ((bit_index < DATA_WIDTH + 1) && (bit_index > 0)) begin
+				txd <= data_buf[bit_index - 1];
+				bit_index <= bit_index + 1;
+			end
+			
+			else if (bit_index == DATA_WIDTH + 1) begin
+				txd <= 1;
+				bit_index <= bit_index + 1;
+			end
+
+			else if (bit_index >= DATA_WIDTH + 1) begin
+				busy <= 0;
+				bit_index <= 0;
+			end
+		end
+	end
+endmodule
+
+
+`default_nettype wire
