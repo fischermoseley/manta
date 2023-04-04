4951ab7e5f41e4327e650a161a170fc70bc41399
Fischer Moseley <42497969+fischermoseley@users.noreply.github.com>
2023-04-03 23:53:33 -0400

    remove files committed by mistake

--- test.py
+++
@@ -1,7 +0,0 @@
-from manta import Manta
-m = Manta('examples/nexys_a7/logic_analyzer/manta.yaml')
-
-hdl = m.my_logic_analyzer.generate_logic_analyzer()
-
-with open("test.v", "w") as f:
-    f.write(hdl)
