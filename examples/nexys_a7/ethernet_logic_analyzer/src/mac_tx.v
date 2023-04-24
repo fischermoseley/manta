0bb3f9c74a63ea6ab2fd9a710434c2140e462d60
Fischer Moseley <42497969+fischermoseley@users.noreply.github.com>
2023-04-24 19:12:28 -0400

    clean up mac_tx, working in simulation

--- examples/nexys_a7/ethernet_logic_analyzer/src/mac_tx.v
+++ examples/nexys_a7/ethernet_logic_analyzer/src/mac_tx.v
@@ -1,192 +1,248 @@
 `default_nettype none
 `timescale 1ns/1ps
 
-module mac_tx(
+module mac_tx (
     input wire clk,
 
+    // TODO: make this variable width
     input wire [15:0] data,
     input wire start,
 
     output reg txen,
-    output reg [1:0] txd
-    );
+    output reg [1:0] txd);
 
-    /*
-    ok so how's this going to work:
-    what goes on the line is either the fcs, or everything else after being routed through bitorder
-
-    we keep the counter, once it gets to some value then we'll flip the switch and talk to the FCS
-
-    so then we mux between:
-        - the fixed portion ahead of the payload (prepayload)
-        - the payload, which is some register
-        - the zero padding, which is of some length that's parameterized
-        - the FCS, which DOES NOT GO THROUGH BITORDER
-
-    */
-
-
+    // packet magic numbers
     localparam PREAMBLE = {7{8'b01010101}};
     localparam SFD = 8'b11010101;
     parameter  SRC_MAC = 48'h69_69_69_69_69_69;
     parameter  DST_MAC = 48'hFF_FF_FF_FF_FF_FF;
-    parameter  LENGTH = 16'h1234;
-    localparam FCS_DATA = 32'b01001110_00010000_01011001_10011010;
-
-    localparam PREPAYLOAD_DATA = {PREAMBLE, SFD, DST_MAC, SRC_MAC, LENGTH};
+    parameter  ETHERTYPE = 16'h1234;
 
     // all lengths are in units of dibits, hence all the mulitplies by four
-    localparam PREPAYLOAD_LEN = (7 + 1 + 6 + 6 + 2) * 4; // in dibits
-    // localparam PAYLOAD_LEN = LENGTH * 4;
+    localparam PREAMBLE_LEN = 7 * 4;
+    localparam SFD_LEN = 1 * 4;
+    localparam SRC_MAC_LEN = 6 * 4;
+    localparam DST_MAC_LEN = 6 * 4;
+    localparam ETHERTYPE_LEN = 2 * 4;
     localparam PAYLOAD_LEN = 2 * 4;
     // localparam ZERO_PAD_LEN = (46 * 4) - PAYLOAD_LEN ; // minimum payload size is 46 bytes
-    localparam ZERO_PAD_LEN = (46 * 4) - PAYLOAD_LEN + 6; // minimum payload size is 46 bytes
-    localparam FCS_LEN = 4*4;
-    localparam IPG_LEN = 96/2;
+    localparam ZERO_PAD_LEN = (46 * 4) - PAYLOAD_LEN + 4; // minimum payload size is 46 bytes
+    localparam FCS_LEN = 4 * 4;
+    localparam IPG_LEN = 96 / 2;
 
-    // state machine
-    reg [8:0] counter = 0;
-    reg [2:0] state = 0;
-    localparam IDLE = 0;
-    localparam PREPAYLOAD = 1;
-    localparam PAYLOAD = 2;
-    localparam ZERO_PAD = 3;
-    localparam FCS = 4;
-    localparam IPG = 5;
-
-    reg prev_start;
-    always @(posedge clk) prev_start <= start;
-
+    // TODO: make crc and bitorder modules not need reset
     reg rst = 1;
     always @(posedge clk) rst <= 0;
 
-
-    reg bitorder_axiiv;
     reg [1:0] bitorder_axiid;
-    reg bitorder_axiov;
     reg [1:0] bitorder_axiod;
+    reg bitorder_axiiv;
+    reg bitorder_axiov;
 
-    bitorder b(
+    bitorder bitorder (
         .clk(clk),
         .rst(rst),
+
         .axiiv(bitorder_axiiv),
         .axiid(bitorder_axiid),
+
         .axiov(bitorder_axiov),
         .axiod(bitorder_axiod));
 
+    reg crc_rst = 1;
     reg crc_axiiv = 0;
-    reg crc_axiov;
     reg [31:0] crc_axiod;
 
-    reg [31:0] fcs = 0;
-
-    crc32 crc(
+    crc32 crc (
         .clk(clk),
-        .rst(rst),
+        .rst(crc_rst),
 
         .axiiv(crc_axiiv),
-        .axiid(txd),
+        .axiid(bitorder_axiod),
 
-        .axiov(crc_axiov),
+        // TODO: remove axiov from crc32 module, it's always valid
+        .axiov(),
         .axiod(crc_axiod));
 
 
-    always @(*) begin
-        if (state == FCS && counter == 0) begin
-            txen <= 1;
-            txd <= {crc_axiod[30], crc_axiod[31]};
-        end
+    // state machine
+    reg [8:0] counter = 0;
+    reg [3:0] state = 0;
 
-        else if (state == FCS && counter != 0) begin
-            txen <= 1;
-            txd <= {fcs[2*(FCS_LEN-counter)-2], fcs[2*(FCS_LEN-counter)-1]};
-        end
-
-        else if (state == IPG) begin
-            txen <= 0;
-            txd <= 0;
-        end
-
-        else begin
-            txen = bitorder_axiov;
-            txd = bitorder_axiod;
-        end
+    localparam IDLE_STATE = 0;
+    localparam PREAMBLE_STATE = 1;
+    localparam SFD_STATE = 2;
+    localparam DST_MAC_STATE = 3;
+    localparam SRC_MAC_STATE = 4;
+    localparam ETHERTYPE_STATE = 5;
+    localparam PAYLOAD_STATE = 6;
+    localparam ZERO_PAD_STATE = 7;
+    localparam FCS_STATE = 8;
+    localparam IPG_STATE = 9;
 
 
-    end
-
+    // sequential logic manages the state machine
     always @(posedge clk) begin
+        counter <= counter + 1;
+        crc_rst <= 0;
 
-        // idle state
-        if (state == IDLE) begin
-            bitorder_axiiv <= 0;
-            bitorder_axiid <= 0;
-
-            if(start && ~prev_start) state <= PREPAYLOAD;
+        if(state == IDLE_STATE) begin
             counter <= 0;
+            crc_axiiv <= 0;
+            if(start) state <= PREAMBLE_STATE;
         end
 
-        // everything before the payload (preamble, sfd, dst mac, src mac, length)
-        else if (state == PREPAYLOAD) begin
-            bitorder_axiiv <= 1;
-            bitorder_axiid <= PREPAYLOAD_DATA[2*(PREPAYLOAD_LEN-counter)-1-:2];
-
-            if(counter == 36) crc_axiiv <= 1;
-
-            counter <= counter + 1;
-            if(counter == PREPAYLOAD_LEN - 1) begin
+        else if(state == PREAMBLE_STATE) begin
+            if(counter == PREAMBLE_LEN - 1) begin
                 counter <= 0;
-                state <= PAYLOAD;
+                state <= SFD_STATE;
             end
         end
 
-        // the payload itself
-        else if (state == PAYLOAD) begin
-            $display(2*(PAYLOAD_LEN-counter)-2);
-            bitorder_axiid <= data[2*(PAYLOAD_LEN-counter)-1-:2];
+        else if(state == SFD_STATE) begin
+            if(counter == SFD_LEN - 1) begin
+                counter <= 0;
+                state <= DST_MAC_STATE;
+            end
+        end
 
-            counter <= counter + 1;
+        else if(state == DST_MAC_STATE) begin
+            // this is because the crc module lags behind the FSM,
+            // as it has to go through bitorder first
+            if(counter == 3) crc_axiiv <= 1;
+
+            if(counter == DST_MAC_LEN - 1) begin
+                counter <= 0;
+                state <= SRC_MAC_STATE;
+            end
+        end
+
+        else if(state == SRC_MAC_STATE) begin
+            if(counter == SRC_MAC_LEN - 1) begin
+                counter <= 0;
+                state <= ETHERTYPE_STATE;
+            end
+        end
+
+        else if(state == ETHERTYPE_STATE) begin
+            if(counter == ETHERTYPE_LEN - 1) begin
+                counter <= 0;
+                state <= PAYLOAD_STATE;
+            end
+        end
+
+        else if(state == PAYLOAD_STATE) begin
             if(counter == PAYLOAD_LEN - 1) begin
                 counter <= 0;
-                state <= ZERO_PAD;
+                state <= ZERO_PAD_STATE;
             end
         end
 
-        // zero padding
-        else if (state == ZERO_PAD) begin
-            bitorder_axiid <= 2'b00;
-
-            counter <= counter + 1;
-            if(counter == ZERO_PAD_LEN - 2) begin
+        else if(state == ZERO_PAD_STATE) begin
+            if(counter == ZERO_PAD_LEN - 1) begin
+                crc_axiiv <= 0;
                 counter <= 0;
-                state <= FCS;
+                state <= FCS_STATE;
             end
         end
 
-
-        // readout fcs from the checksum module
-        else if (state == FCS) begin
-
-            if(counter == 0) fcs <= crc_axiod;
-
-            counter <= counter + 1;
+        else if(state == FCS_STATE) begin
             if(counter == FCS_LEN - 1) begin
                 counter <= 0;
-                state <= IPG;
+                state <= IPG_STATE;
             end
         end
 
-        // interpacket gap
-        else if (state == IPG) begin
-            bitorder_axiiv <= 0;
-            bitorder_axiid <= 2'b00;
-
-            counter <= counter + 1;
-            if (counter == IPG_LEN - 1) begin
+        else if(state == IPG_STATE) begin
+            if(counter == IPG_LEN - 1) begin
+                crc_rst <= 1;
                 counter <= 0;
-                state <= IDLE;
+                state <= IDLE_STATE;
             end
         end
     end
+
+    // combinational logic handles the pipeline
+    always @(*) begin
+        case (state)
+            IDLE_STATE: begin
+                bitorder_axiiv = 0;
+                bitorder_axiid = 0;
+                txen = 0;
+                txd = 0;
+            end
+
+            PREAMBLE_STATE: begin
+                bitorder_axiiv = 1;
+                bitorder_axiid = PREAMBLE[2*(PREAMBLE_LEN-counter)-1-:2];
+                txen = bitorder_axiov;
+                txd = bitorder_axiod;
+            end
+
+            SFD_STATE: begin
+                bitorder_axiiv = 1;
+                bitorder_axiid = SFD[2*(SFD_LEN-counter)-1-:2];
+                txen = bitorder_axiov;
+                txd = bitorder_axiod;
+            end
+
+            DST_MAC_STATE: begin
+                bitorder_axiiv = 1;
+                bitorder_axiid = DST_MAC[2*(DST_MAC_LEN-counter)-1-:2];
+                txen = bitorder_axiov;
+                txd = bitorder_axiod;
+            end
+
+            SRC_MAC_STATE: begin
+                bitorder_axiiv = 1;
+                bitorder_axiid = SRC_MAC[2*(SRC_MAC_LEN-counter)-1-:2];
+                txen = bitorder_axiov;
+                txd = bitorder_axiod;
+            end
+
+            ETHERTYPE_STATE: begin
+                bitorder_axiiv = 1;
+                bitorder_axiid = ETHERTYPE[2*(ETHERTYPE_LEN-counter)-1-:2];
+                txen = bitorder_axiov;
+                txd = bitorder_axiod;
+            end
+
+            PAYLOAD_STATE: begin
+                bitorder_axiiv = 1;
+                bitorder_axiid = data[2*(PAYLOAD_LEN-counter)-1-:2];
+                txen = bitorder_axiov;
+                txd = bitorder_axiod;
+            end
+
+            ZERO_PAD_STATE: begin
+                bitorder_axiiv = 1;
+                bitorder_axiid = 0;
+                txen = bitorder_axiov;
+                txd = bitorder_axiod;
+            end
+
+            FCS_STATE: begin
+                bitorder_axiiv = 0;
+                bitorder_axiid = 0;
+                txen = 1;
+                txd = {crc_axiod[2*(FCS_LEN-counter)-2], crc_axiod[2*(FCS_LEN-counter)-1]};
+            end
+
+            IPG_STATE: begin
+                bitorder_axiiv = 0;
+                bitorder_axiid = 0;
+                txen = 0;
+                txd = 0;
+            end
+
+            default: begin
+                bitorder_axiiv = 0;
+                bitorder_axiid = 0;
+                txen = 0;
+                txd = 0;
+            end
+        endcase
+    end
 endmodule
+
 `default_nettype wire
