b5010285b5f45a42b7a642b3cadd0d19f339c1a5
Fischer Moseley <42497969+fischermoseley@users.noreply.github.com>
2023-08-21 15:08:38 -0700

    add block_mem_uart example

---
+++ examples/nexys_a7/block_mem_uart/api_example.py
@@ -0,0 +1,18 @@
+from manta import Manta
+from time import sleep
+from random import randint
+
+m = Manta('manta.yaml')
+
+for addr in range(1024):
+    number = randint(0,65535)
+    m.my_block_memory.write(addr, number)
+
+    readback = m.my_block_memory.read(addr)
+
+    if readback == number:
+        print(f"Success! Wrote and read back {number} from {addr}")
+
+    else:
+        print(f"Failure! Wrote {number} to {addr}, but received {readback}")
+        exit()
