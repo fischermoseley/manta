1d9e643c053da7710e1831db6d2ec7512fe5a892
Fischer Moseley <42497969+fischermoseley@users.noreply.github.com>
2024-01-11 09:32:36 -0800

    fix bug in USB PID/VID reporting

--- src/manta/__init__.py
+++ src/manta/__init__.py
@@ -332,7 +332,7 @@ Supported commands:
 
             # sometimes macOS will enumerate non-serial devices as serial ports,
             # in which case the PID/VID/serial/location/etc are all None
-            pid = f"0x{port.vid:04X}" if port.pid is not None else "None"
+            pid = f"0x{port.pid:04X}" if port.pid is not None else "None"
             vid = f"0x{port.vid:04X}" if port.vid is not None else "None"
 
             print(f" ->  pid: {pid}")
